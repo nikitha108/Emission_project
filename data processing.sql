6bc8eac272bbdb4d8bd5f5373185d9de19bafc24
Nikitha Sathish <nikithasathish108@gmail.com>
2025-01-10 15:26:45 +0100

    add readme

--- data processing.sql
+++ data processing.sql
@@ -1,1 +1,10 @@
-n
+SELECT COUNT(*) FROM emissions_data;
+
+
+-- Check for NULL values in key columns     
+SELECT COUNT(*) FROM emissions_data
+WHERE gdp IS NULL;
+
+-- Population and co2 columns are non- null values while we have 916 gdp columns null
+
+
